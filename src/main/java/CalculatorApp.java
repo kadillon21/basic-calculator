cdf718637c1fb755972cb0dd19f6723acfa498a5
kadillon21 <kadillon21@gmail.com>
2026-04-09 15:36:29 -0500

    Multiples the two digits provided

---
+++ src/main/java/CalculatorApp.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class CalculatorApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int x = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int y = scanner.nextInt();
+
+
+        System.out.println("Possible calculations:");
+        System.out.println("  (A)dd");
+        System.out.println("  (S)ubtract");
+        System.out.println("  (M)ultiply");
+        System.out.println("  (D)ivide");
+        System.out.println("Please select and option: ");
+
+        String choice = scanner.nextLine();
+
+        System.out.println(x + " * " + y + " = " + (x * y));
+    }
+
+
+}
